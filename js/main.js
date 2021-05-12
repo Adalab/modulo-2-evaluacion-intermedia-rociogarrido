a45bc2773b551ae0e2da2e3749530d438f3dc7ba
Rocío Garrido <rocioganon@gmail.com>
2021-05-12 09:35:10 +0200

    create variables js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Crear constantes para acceder a los elementos del HTML.
+
+const inputNumber = document.querySelector(".js-number");
+const button = document.querySelector(".js-button");
+const clue = document.querySelector(".js-clue");
+const attempts = document.querySelector(".js-counter");
