c386e7760f3336f703d575a041daa610bfaaf3de
Rocío Garrido <rocioganon@gmail.com>
2021-05-17 11:28:01 +0200

    add parseInt

--- js/main.js
+++ js/main.js
@@ -5,19 +5,21 @@ const button = document.querySelector(".js-button");
 const clue = document.querySelector(".js-clue");
 const attempts = document.querySelector(".js-counter");
 
+// PROCESO 1
 // 2. Generar un número aleatorio con la ayuda de Math.random y Math.ceil. Para ello, copiad estas 3 líneas de código en vuestro JS y no las modifiquéis:
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-let randomNumber = getRandomNumber(100); // variable para asignarle el parámetro máximo 100 a la función anterior.
+const randomNumber = getRandomNumber(100); // variable para asignarle el parámetro máximo 100 a la función anterior y ejecutarla.
 
 // 3. Mostrar en consola el número aleatorio para saber qué número se ha generado.
 console.log('Mi número aleatorio es ' + randomNumber);
 
 // 5. Comparar el número que la jugadora ha escrito en el input con el número aleatorio 
 function checkNumber () {
-    const userGuess = inputNumber.value;
+    // creo una constante para coger el valor del input y transformarlo a número entero
+    const userGuess = parseInt(inputNumber.value);
   // y pintar las pistas correspondientes en la pantalla.
     if (userGuess === '') {  
         clue.innerHTML = "El número debe estar entre 1 y 100."; 
@@ -31,7 +33,7 @@ function checkNumber () {
         clue.innerHTML = "¡¡¡Enhorabuena, campeona!!!";
     }
 }
-
+// PROCESO 2: CONTAR EL NÚMERO DE INTENTOS
 // 6. Actualizar el contador de intentos cada vez que la jugadora pruebe.
 let guessCount = 0; // Para poner el contador a cero.
 function updateAttempts() {
@@ -39,7 +41,7 @@ function updateAttempts() {
   attempts.innerHTML = "Número de intentos: " + guessCount;
 }
 
-// Función para que se ejecuten a la vez las dos funciones anteriores.
+// Función manejadora para llamar a las dos funciones anteriores al hacer "click".
 function handleGuessNumber(ev) {
     ev.preventDefault(); // para que la página no se actualice con cada click
     checkNumber();
